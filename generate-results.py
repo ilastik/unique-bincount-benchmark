f575246e3b66ac7d01c7162cd00ba576d7022e6c
k-dominik <k-dominik@users.noreply.github.com>
2024-08-30 10:15:02 +0200

    add normalized runningtime plots

--- generate-results.py
+++ generate-results.py
@@ -38,6 +38,46 @@ def gen_plots():
 
     p.clear()
 
+    # normalize data
+    # method    shape   t   py_version  npy_version pandas_version  vigra_version   host    platform    mem_min mem_max
+    np_unique = results_unique[results_unique["method"] == "numpy.unique(data)"]
+
+    normalization_factors = {}
+
+    for _, row in np_unique.iterrows():
+        key = (
+            row["host"],
+            row["platform"],
+            row["shape"],
+            row["py_version"],
+            row["npy_version"],
+            row["pandas_version"],
+            row["vigra_version"],
+        )
+        normalization_factors[key] = row["t"]
+
+    def normalize(row):
+        key = (
+            row["host"],
+            row["platform"],
+            row["shape"],
+            row["py_version"],
+            row["npy_version"],
+            row["pandas_version"],
+            row["vigra_version"],
+        )
+        return row["t"] / normalization_factors[key]
+
+    results_unique["normalized_t"] = results_unique.apply(normalize, axis=1)
+
+    p = sns.lineplot(x="shape_ind", y="normalized_t", hue="method", data=results_unique)
+    p.set(yscale="log")
+    fig = p.get_figure()
+    fig.savefig("unique-runningtime-normalized.png")
+
+    p.clear()
+
+
     results_bincount = pandas.read_csv("results-bincount.csv", sep="\t")
 
     results_bincount["shape_ind"] = results_bincount["shape"].map(shape_index_mapping)
@@ -55,6 +95,46 @@ def gen_plots():
     fig.savefig("bincount-max-memory.png")
 
     p.clear()
+
+    # normalize data
+    # method    shape   t   py_version  npy_version pandas_version  vigra_version   host    platform    mem_min mem_max
+    np_bincount = results_bincount[results_bincount["method"] == "numpy.bincount(data.reshape(-1))"]
+
+    normalization_factors = {}
+
+    for _, row in np_bincount.iterrows():
+        key = (
+            row["host"],
+            row["platform"],
+            row["shape"],
+            row["py_version"],
+            row["npy_version"],
+            row["pandas_version"],
+            row["vigra_version"],
+        )
+        normalization_factors[key] = row["t"]
+
+    def normalize(row):
+        key = (
+            row["host"],
+            row["platform"],
+            row["shape"],
+            row["py_version"],
+            row["npy_version"],
+            row["pandas_version"],
+            row["vigra_version"],
+        )
+        return row["t"] / normalization_factors[key]
+
+    results_bincount["normalized_t"] = results_bincount.apply(normalize, axis=1)
+
+    p = sns.lineplot(x="shape_ind", y="normalized_t", hue="method", data=results_bincount)
+    p.set(yscale="log")
+    fig = p.get_figure()
+    fig.savefig("bincount-runningtime-normalized.png")
+
+    p.clear()
+
 
 
 def main():
